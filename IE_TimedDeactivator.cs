d46235c53c3de6393968f7ebdde5fc099488d54f
Inan Evin <inanevin@gmail.com>
2018-09-27 18:41:49 +0300

    Timed deactivator.

---
+++ IE_TimedDeactivator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IE_TimedDeactivator : MonoBehaviour
+{
+
+    public float f_Time;
+    public bool b_RunsOnEnable;
+
+    void OnEnable()
+    {
+        if (b_RunsOnEnable) PVO_Deactivate();
+    }
+
+    public void PVO_Deactivate()
+    {
+        Invoke("Deac", f_Time);
+    }
+
+    void Deac()
+    {
+        gameObject.SetActive(false);
+    }
+}
