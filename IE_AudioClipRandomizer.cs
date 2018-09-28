147df1efd1877427533638a39ae8661615a579fd
Inan Evin <inanevin@gmail.com>
2018-09-28 21:11:34 +0300

    Audio Clip Randomizer Added.

---
+++ IE_AudioClipRandomizer.cs
@@ -0,0 +1,25 @@
+﻿/*
+Author: Inan Evin
+www.inanevin.com
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class IE_AudioClipRandomizer : MonoBehaviour
+{
+
+    private AudioSource as_This;
+    public AudioClip[] ac_Clips;
+
+    void Awake()
+    { as_This = GetComponent<AudioSource>(); }
+	
+    void OnEnable()
+    {
+        AudioClip clip = ac_Clips[Random.Range(0, ac_Clips.Length)];
+        as_This.PlayOneShot(clip);
+    }
+}
